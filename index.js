fa21c909895542cca2b6032a1b3700aa11e4aebb
mrgregorien <mrwilliamgregorio@gmail.com>
2022-08-14 22:11:04 -0400

    Great white shark we do

---
+++ index.js
@@ -0,0 +1,11 @@
+//great white shark
+const greatWhiteShark = {
+    myKingdom: 'Anamalia',
+    myPhylum: 'Chordata',
+    myClass: 'Chondrichthyes',
+    myOrder: 'Lamniformes',
+    myFamily: 'Charcharodon',
+    mySpecies: 'Charcharias'
+}
+
+console.log(greatWhiteShark);
